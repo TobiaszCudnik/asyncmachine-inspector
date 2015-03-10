92f4bdc237ebe54348b269e7bcfa6f455a879fc5
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2015-03-11 00:07:24 +0100

    d3 graph

--- src/visualizer.js
+++ src/visualizer.js
@@ -2,92 +2,170 @@ import am from 'asyncmachine'
 import Graph from 'graphlib/lib/graph'
 import uuid from 'node-uuid'
 import assert from 'assert'
+import d3 from 'd3'
 
 export class Node {
 
   constructor(name, machine) {
-    this.id = uuid.v4()
-    this.machine = machine
-    this.name = name
+	this.id = uuid.v4()
+	this.machine = machine
+	this.name = name
   }
 
   get() {
-    return this.machine.get(this.name)
+	return this.machine.get(this.name)
   }
 }
 
-export class VisualizerStates extends am.AsyncMachine {
-}
 
-
-export class Visualizer {
+export class Network {
 
   constructor() {
-    this.graph = new Graph
-    this.states = new VisualizerStates
-    this.nodes = {}
-    this.events = []
+	this.graph = new Graph
+	this.nodes = {}
+	this.events = []
   }
 
   addMachine(machine) {
-    this.getNodesFromMachine(machine)
-    this.bindToMachine(machine)
-    this.scanReferences()
+	this.getNodesFromMachine(machine)
+	this.bindToMachine(machine)
+	this.scanReferences()
   }
 
 
   bindToMachine(machine) {
-    // TODO override event triggers to apply them on the UI immediate
+	// TODO override event triggers to apply them on the UI immediate
   }
 
 
   getNodesFromMachine(machine) {
-    // scan states
-    let new_nodes = []
-    for (let name of machine.states_all) {
-      let node = new Node(name, machine)
-      this.nodes[node.id] = node
-      this.graph.setNode(node.id, node)
-      new_nodes.push(node)
-    }
+	// scan states
+	let new_nodes = []
+	for (let name of machine.states_all) {
+	  let node = new Node(name, machine)
+	  this.nodes[node.id] = node
+	  this.graph.setNode(node.id, node)
+	  new_nodes.push(node)
+	}
 
-    // get edges from relations
-    for (let node of new_nodes)
-      this.getRelationsFromNode(node, machine)
+	// get edges from relations
+	for (let node of new_nodes)
+	  this.getRelationsFromNode(node, machine)
   }
 
   getRelationsFromNode(node, machine) {
-    // TODO limit to 'requires' and 'drops' ?
-    let state = node.get()
-    assert(state)
-    for (let relation in state) {
-      if (relation == 'auto')
-        continue
+	// TODO limit to 'requires' and 'drops' ?
+	let state = node.get()
+	assert(state)
+	for (let relation in state) {
+	  if (relation == 'auto')
+		continue
 
-      let targets = state[relation]
+	  let targets = state[relation]
 
-      for (let target_name of targets) {
-        let target = this.getNodeByName(target_name, machine)
-        assert(target)
-        this.graph.setEdge(node.id, target.id, relation)
-      }
-    }
+	  for (let target_name of targets) {
+		let target = this.getNodeByName(target_name, machine)
+		assert(target)
+		this.graph.setEdge(node.id, target.id, relation)
+	  }
+	}
   }
 
   getNodeByName(name, machine) {
-    for (let id in this.nodes) {
-      let node = this.nodes[id]
-      if (node.name === name && node.machine === machine)
-        return node
-    }
+	for (let id in this.nodes) {
+	  let node = this.nodes[id]
+	  if (node.name === name && node.machine === machine)
+		return node
+	}
   }
 
   scanReferences() {
-    // scan listeners, get edges from listener refs
+	// scan listeners, get edges from listener refs
   }
 }
 
 
 export class VisualizerUi {
 
+	constructor(network) {
+
+        this.network = network
+
+        this.width = 460,
+        this.height = 250;
+
+        this.color = d3.scale.category20()
+
+        this.force = d3.layout.force()
+            .charge(-120)
+            .linkDistance(30)
+            .size([this.width, this.height])
+
+        this.container = d3.select("body").append("svg")
+            .attr("width", this.width)
+            .attr("height", this.height)
+    }
+
+    get nodes() {
+        return this.network.graph.nodes().map( id => {
+            return this.network.nodes[id]
+        })
+    }
+
+    get links() {
+        var nodes = this.network.graph.nodes()
+        return this.network.graph.edges().map( edge => {
+            return {
+                source: nodes.indexOf(edge.v),
+                target: nodes.indexOf(edge.w),
+                value: 1
+            }
+        })
+    }
+
+	render() {
+        var nodes = this.nodes
+        var links = this.links
+
+        this.force
+          .nodes(nodes)
+          .links(links)
+          .start()
+
+        var link = this.container.selectAll(".link")
+            .data(links)
+            .enter().append("line")
+            .attr("class", "link")
+            .style("stroke-width", d => {
+                return d.value
+        })
+
+        var node = this.container.selectAll(".node")
+            .data(nodes)
+            .enter().append("g")
+            .attr("class", "node")
+            .call(this.force.drag)
+
+        node.append("circle").attr("r", 4.5)
+        node.append("text")
+            .attr("dx", 12)
+            .attr("dy", ".35em")
+            .text(function(d) { return d.name });
+
+        node.append("title")
+          .text( d => { return d.name; });
+
+        this.force.on("tick", () => {
+            link.attr("x1",  d => { return d.source.x; })
+                .attr("y1",  d => { return d.source.y; })
+                .attr("x2",  d => { return d.target.x; })
+                .attr("y2",  d => { return d.target.y; });
+
+            //node.attr("cx",  d => { return d.x; })
+            //    .attr("cy",  d => { return d.y; })
+            node.attr("transform", (d) => {
+                return "translate(" + d.x + "," + d.y + ")"
+            })
+        })
+	}
 }
--- test/visualizer.js
+++ test/visualizer.js
@@ -1,7 +1,7 @@
 import { AsyncMachine } from 'asyncmachine'
 import { expect } from 'chai'
 import _ from 'underscore'
-import { Visualizer } from '../src/visualizer'
+import { Network, VisualizerUi } from '../src/visualizer'
 
 
 describe("Single machine graph", function() {
@@ -12,17 +12,17 @@ describe("Single machine graph", function() {
     this.machine.C = {drops: ['B']}
     this.machine.D = {requires: ['C']}
 
-    this.vis = new Visualizer
-    this.vis.addMachine(this.machine)
+    this.network = new Network
+    this.network.addMachine(this.machine)
   })
 
   it('should get all states as nodes', function() {
-    expect(this.vis.graph.nodes().length).to.be.eql(5)
+    expect(this.network.graph.nodes().length).to.be.eql(5)
   })
 
   it('should get all relations as edges', function() {
-    var nodes = this.vis.nodes
-    let edges = _.map(this.vis.graph.edges(), edge => {
+    var nodes = this.network.nodes
+    let edges = _.map(this.network.graph.edges(), edge => {
       return `${nodes[edge.v].name} ${nodes[edge.w].name}`
     })
     expect(edges).to.eql([
@@ -30,5 +30,18 @@ describe("Single machine graph", function() {
       'C B',
       'D C'
     ])
+  })
+
+
+  describe('ui', function() {
+    it('should render', function() {
+      var ui = new VisualizerUi(this.network)
+      ui.render()
+    })
+
+    afterEach(function() {
+      "use strict";
+
+    })
   })
 })
