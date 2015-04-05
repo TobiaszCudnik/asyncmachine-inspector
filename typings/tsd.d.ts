3eaa9564fe45b8e4645965f082cec0587a0fbf87
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2015-04-06 00:12:46 +0200

    typings

---
+++ typings/tsd.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="assert/assert.d.ts" />
+/// <reference path="d3/d3.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="node-uuid/node-uuid.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="node/node.d.ts" />
