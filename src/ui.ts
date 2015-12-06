31ade2e37343da48172187f4018f866295a0f635
Tobiasz Cudnik <tobiasz.cudnik@voiceworks.com>
2015-12-06 21:57:52 +0100

    single for layout for all state machines

--- src/ui.ts
+++ src/ui.ts
@@ -1,36 +1,14 @@
-import StateGraph, {
-	Node,
-	ExternalNode
-} from './stategraph'
 import d3 from 'd3'
 import _ from 'lodash'
 import * as am from 'asyncmachine'
 import Graph from 'graphs'
-
-export interface D3StateGraph extends StateGraph {
-	graph: Graph<D3Node>
-}
-
-export interface D3AsyncMachine extends am.AsyncMachine, D3.Geom.Point {
-	x: number;
-	y: number;
-}
-
-export class D3Node extends Node implements D3.Geom.Point {
-	machine: D3AsyncMachine;
-    externals: D3ExternalNode[];
-	x: number;
-	y: number;
-}
-
-export class D3ExternalNode implements ExternalNode, D3.Geom.Point {
-	x: number;
-	y: number;
-	constructor(
-		public node: D3Node,
-		public machine: D3AsyncMachine
-		){}
-}
+import {
+	D3Node,
+	D3Machine,
+	D3NodeLink,
+	OBJECT_TYPE,
+	NODE_LINK_TYPE
+} from './d3stategraph'
 
 /**
  * TODO consume a stream of events
@@ -38,177 +16,70 @@ export class D3ExternalNode implements ExternalNode, D3.Geom.Point {
  */
 export default class Ui {
 
-	stateGraph: D3StateGraph;
 	color: D3.Scale.OrdinalScale;
 	node_color: D3.Scale.OrdinalScale;
-	width: number;
-	height: number;
 	layout: D3.Layout.ForceLayout;
-	node_layouts: Map<am.AsyncMachine, D3.Layout.ForceLayout>;
 	container: D3.Selection;
 	links: D3.Layout.GraphLinkForce[];
 	machine_nodes: D3.Selection;
 	ticksPerRender = 3;
+	width = 800;
+	height = 600;
 
-	constructor(stateGraph: D3StateGraph) {
-
-		this.stateGraph = stateGraph
-
-		this.width = 800,
-		this.height = 600;
+	constructor(
+			public data: D3StateGraph) {
 
 		this.color = d3.scale.category20()
 
 		this.layout = d3.layout.force()
 			.charge(-120)
-			.linkDistance(200)
+			.linkDistance(this.linkDistance.bind(this))
 			.size([this.width, this.height])
+			.nodes(this.data.nodes)
+			.links(this.data.links)
+
 		this.node_color = d3.scale.category20c();
 
-		this.node_layouts = new Map<am.AsyncMachine,
-			D3.Layout.ForceLayout>();
-			
-		this.machines.forEach( (machine) => {
-			let size = machine.states_all.length * 8
-			var layout = d3.layout.force()
-				.charge(-60)
-				.linkDistance(50)
-				.size([size, size])
+		//let size = machine.states_all.length * 8
+		//var layout = d3.layout.force()
+		//	.charge(-60)
+		//	.linkDistance(50)
+		//	.size([size, size])
 
-			// redraw on changes
-			machine.on('change', layout.tick.bind(layout))
-			this.node_layouts.set(machine, layout)
-		})
-
-		this.container = d3.select("body").append("svg")
-			.attr("width", this.width)
-			.attr("height", this.height)
-	}
-
-	get machines(): D3AsyncMachine[] {
-		return <D3AsyncMachine[]>_.chain(this.stateGraph.nodes)
-			.pluck('machine').uniq().value()
-	}
-
-	nodes(machine) {
-		var nodes = [];
-
-		this.stateGraph.graph.forEach(node => {
-			if (node.machine !== machine)
-				return
-
-			nodes.push(node)
-			// Collect external nodes to which the current node points to
-			var links_from = this.stateGraph.graph.from(node)
-			for (let target of links_from) {
-				if (target.machine !== machine) {
-					node.externals.push(new D3ExternalNode(
-						target, node.machine
-					))
-				}
-			}
-
-			// Collect external nodes pointing to this one
-			for (let source of this.stateGraph.graph.to(node)) {
-				if (source.machine !== machine && !links_from.has(source)) {
-					node.externals.push(new D3ExternalNode(
-						source, node.machine
-					))
-				}
-			}
-
-			nodes.push.apply(nodes, node.externals)
-		})
-
-		return nodes;
-	}
-
-	get machine_links(): D3.Layout.GraphLinkForce[] {
-		var links = [];
-
-		this.stateGraph.graph.traverse( (from, to) => {
-			if (from.machine == to.machine)
-				return
-
-			// TODO duplicates
-			links.push({
-				source: from.machine,
-				target: to.machine,
-				value: 1
-			})
-		})
-
-		return links
-	}
-
-	node_links(machine) {
-		var links: D3.Layout.GraphLinkForce[] = []
-
-		this.stateGraph.graph.traverse( (from, to) => {
-			if (to.machine !== from.machine && to.machine === machine) {
-				for (let external of to.externals) {
-					if (external.node == to || external.node == from) {
-						links.push({
-							source: external,
-							target: to,
-							value: 1
-						})
-					}
-				}
-			}			
-
-			if (from.machine !== machine)
-				return
-
-			if (to.machine !== from.machine) {
-				for (let external of from.externals) {
-					if (external.node == to || external.node == from) {
-						var target = external
-						// TODO support more
-						break
-					}
-				}
-			} else
-				var target = to
-
-			links.push({
-				source: from,
-				target: target,
-				value: 1
-			})
-		})
-
-		return links
 	}
 
 	render() {
+		// TODO customizable container
+		this.container = d3.select("body").append("svg")
+			.attr("width", this.width)
+			.attr("height", this.height)
 		this.renderMachineNodes()
 		this.renderStateNodes()
 	}
 
+	linkDistance(link) {
+		switch(link.source.object_type) {
+			case OBJECT_TYPE.MACHINE:
+				return 5
+			break;
+			case OBJECT_TYPE.STATE:
+				return (link.source.machine_id == link.target.machine_id)
+					? 50 : 250;
+			break;
+		}
+	}
+
 	renderMachineNodes() {
-		var machines = this.machines
-		var links = this.machine_links
 		var color = d3.scale.category20();
 
-		this.layout
-			// TODO support the ID getter
-			.nodes(machines)
-			.links(links)
-			//.on('start', () => {
-			//	requestAnimationFrame(this.redrawMachineNodes.bind(this))
-			//})
-
 		this.machine_nodes = this.container.selectAll(".node.machine")
-			.data(machines)
+			.data(this.data.nodes.filter( node => {
+				return node.object_type == OBJECT_TYPE.MACHINE
+			}))
 			.enter().append("g")
 				.attr("class", "node machine")
-				.attr("id", d => { return this.stateGraph.machine_id(d) })
+				.attr("id", d => { return d.id })
 				.call(this.layout.drag)
-
-		this.machine_nodes.append("circle")
-			.attr("r", d => d.states_all.length * 20)
-			.style("fill", d => { return color(this.stateGraph.machine_id(d)) })
 
 		this.machine_nodes.append("text")
 			.attr("dx", 12)
@@ -220,179 +91,115 @@ export default class Ui {
 
 		this.layout.start()
 			.on('tick', () => {
-				requestAnimationFrame(this.redrawMachineNodes.bind(this))
+				requestAnimationFrame(this.redrawNodes.bind(this))
 			})
 	}
 
-	redrawMachineNodes() {
+	redrawNodes() {
 		this.machine_nodes.attr("transform", (d) => {
+			// TODO move the circle
+			//this.machine_nodes.append("circle")
+			//	.attr("r", node => {
+			//		var children_count = this.data.links.filter( link => {
+			//			return link.source_id == node.id
+			//		}).length
+			//		// TODO to the config
+			//		return 20 * children_count
+			//	})
+			//	.style("fill", d => { return color(d.id) })
 			return "translate(" + d.x + "," + d.y + ")"
 		})
-	}
-
-	renderStateNodes() {
-		this.machines.forEach( (machine: am.AsyncMachine) => {
-			var nodes = this.nodes(machine)
-			var node_links = this.node_links(machine)
-			var layout = this.node_layouts.get(machine)
-
-			layout
-				.nodes(nodes)
-				.links(node_links)
-				//.on('start', () => {
-				//	requestAnimationFrame(this.redrawStateNodes.bind(this, machine, node, link))
-				//})
-
-			var node = this.container.select("#" + this.stateGraph.machine_id(machine))
-				.selectAll('.nodes')
-				.data(nodes)
-				.enter().append("g")
-				.attr("class", "node")
-				//.call(this.layout.drag)
-
-			var circle = node.append("circle")
-				.attr("r", d => { return d.node ? 2 : 7 } )
-				.style("stroke", d => { return d.node ? 'transparent' : 'white' })
-
-			var link = this.container.select("#" + this.stateGraph.machine_id(machine))
-				.selectAll(".link")
-				.data(node_links)
-				.enter().append("line")
-				// TODO use a separate color per relation
-				.attr("class", "link")
-				.style("stroke-width", d => { return d.value } )
-
-			node.append("text")
-				.attr("dx", (d) => {
-					var name = d.node ? d.node.name : d.name
-					return `-${name.length / 5}em`
-				})
-				.attr("dy", 25)
-				.text(function(d) { return d.name });
-
-			node.append("title")
-				.text( d => { return d.name; });
-
-			layout.start()
-				.on('tick', () => {
-					requestAnimationFrame(this.redrawStateNodes.bind(this, machine, node, link, circle))
-				})
-		})
-	}
-
-	redrawStateNodes(machine, node, link, circle) {
-		//for (var i = 0; i < this.ticksPerRender; i++) {
-		//	this.node_layouts.get(machine).tick();
-		//}
-
-		var q = d3.geom.quadtree(this.machines),
-			i = 0,
-			n = this.machines.length;
-
-		while (++i < n)
-			q.visit(collide(this.machines[i]));
-
-		// update the position
-		node.attr("transform", (d) => {
-			return "translate(" + (d.x - machine.states_all.length * 4) + "," +
-				(d.y - machine.states_all.length * 4) + ")"
+		this.state_nodes.attr("transform", (d) => {
+			return "translate(" + d.x + "," + d.y + ")"
 		})
 
-		// update the color
-		circle.style("fill", d => {
-			return d.node ? 'transparent' : (d.state ? 'red' : 'blue')
-		})
-
-		// update the edge coordinates
-		link
-			.attr("x1", this.linkCoords.bind(null, 'x1'))
-			.attr("x2", this.linkCoords.bind(null, 'x2'))
-			.attr("y1", this.linkCoords.bind(null, 'y1'))
-			.attr("y2", this.linkCoords.bind(null, 'y2'))
-			.attr("opacity", function(d) {
-				if (d.source.node && this.style) {
+		this.links
+			.attr("x1", link => link.source.x)
+			.attr("x2", link => link.target.x)
+			.attr("y1", link => link.source.y)
+			.attr("y2", link => link.target.y)
+			.attr("opacity", function(link) {
+				if (link.type == NODE_LINK_TYPE.PIPED_IN) {
 					this.style.setProperty('opacity', 0)
 				}
 			})
-
-		this.node_layouts.get(machine).alpha(.1)
 	}
 
-	linkCoords(coord, d: {
-			source: D3Node | D3ExternalNode,
-			target: D3Node | D3ExternalNode
-			}) {
-		var circle_correction = d.source.machine.states_all.length * 4
+	renderStateNodes() {
+		this.state_nodes = this.container.selectAll('.node.state')
+			.data(this.data.nodes.filter( node => {
+				return node.object_type == OBJECT_TYPE.STATE
+			}))
+			.enter()
+				.append("g")
+				.attr("class", "node state")
+				.call(this.layout.drag)
 
-		if (d.source instanceof D3ExternalNode) {
-			var external = d.source.node
-			var external_circle_correction = external.machine.states_all.length * 4
+		var circle = this.state_nodes.append("circle")
+			.attr("r", d => { return d.node ? 2 : 7 } )
+			.style("stroke", d => { return d.node ? 'transparent' : 'white' })
 
-			var x2 = -d.source.machine.x + external.machine.x + external.x - external_circle_correction
-			var y2 = -d.source.machine.y + external.machine.y + external.y - external_circle_correction
+		var links = this.data.links.filter( link => {
+			return link.object_type == OBJECT_TYPE.STATE_LINK
+		})
+		this.links = this.container
+			.selectAll(".link")
+			.data(links)
+			.enter()
+				.append("line")
+				// TODO use a separate color per relation
+				.attr("class", "link")
+				.style("stroke-width", 5 )
 
-			d.source.x = Math.max(-circle_correction*5,
-				Math.min(circle_correction*5, x2)) + circle_correction
-			d.source.y = Math.max(-circle_correction*5,
-				Math.min(circle_correction*5, y2)) + circle_correction
+		this.state_nodes.append("text")
+			.attr("dx", (d) => {
+				return `-${d.name.length / 5}em`
+			})
+			.attr("dy", 25)
+			.text(function(d) { return d.name });
 
-			return
-		}
-		
-		if (d.target instanceof Node && d.source instanceof Node) {
-			switch(coord) {
-				case 'x1':
-					return d.source.x - circle_correction
-					break;
-				case 'y1':
-					return d.source.y - circle_correction
-					break;
-				case 'x2':
-					return d.target.x - circle_correction
-					break;
-				case 'y2':
-					return d.target.y - circle_correction
-					break;
-			}
-		}
-
-		if (d.target instanceof D3ExternalNode) {
-			var external = d.target.node
-			var external_circle_correction = external.machine.states_all.length * 4
-	
-			var x2 = -d.source.machine.x + external.machine.x + external.x - external_circle_correction
-			var y2 = -d.source.machine.y + external.machine.y + external.y - external_circle_correction
-	
-	// 		var fn = function(x, x1, y1, x2, y2) {
-	// 			return y1 + ( (y2 - y1) / (x2 - x1) ) * (x - x1)
-	// 		}
-	
-			var range = 50
-	
-			d.target.x = Math.max(-circle_correction*5,
-				Math.min(circle_correction*5, x2)) + circle_correction
-			d.target.y = Math.max(-circle_correction*5,
-				Math.min(circle_correction*5, y2)) + circle_correction
-	// 		d.target.y = fn(d.source.machine.y + circle_correction, d.source.machine.x, d.source.machine.y, x2, y2)
-	
-			switch(coord) {
-				case 'x1':
-					return d.source.x - circle_correction
-					break;
-				case 'y1':
-					return d.source.y - circle_correction
-					break;
-				case 'x2':
-					return x2
-					break;
-				case 'y2':
-					return y2
-					break;
-			}
-
-			// TODO position the fake node on the circle boundry
-		}
+		this.state_nodes.append("title")
+			.text( d => { return d.name; });
 	}
+
+	//redrawStateNode(machine, node, link, circle) {
+	//	//for (var i = 0; i < this.ticksPerRender; i++) {
+	//	//	this.node_layouts.get(machine).tick();
+	//	//}
+    //
+	//	var q = d3.geom.quadtree(this.machines),
+	//		i = 0,
+	//		n = this.machines.length;
+    //
+	//	while (++i < n)
+	//		q.visit(collide(this.machines[i]));
+    //
+	//	// update the position
+	//	node.attr("transform", (d) => {
+	//		return "translate(" + (d.x - machine.nodes.length * 4) + "," +
+	//			(d.y - machine.nodes.length * 4) + ")"
+	//	})
+    //
+	//	// update the color
+	//	circle.style("fill", d => {
+	//		return d.object_type == OBJECT_TYPE.NODE_EXTERNAL ? 'transparent' : (d.state ? 'red' : 'blue')
+	//	})
+    //
+	//	// update the edge coordinates
+	//	link
+	//		// TODO string enum
+	//		.attr("x1", this.linkCoords.bind(null, 'x1', machine))
+	//		.attr("x2", this.linkCoords.bind(null, 'x2', machine))
+	//		.attr("y1", this.linkCoords.bind(null, 'y1', machine))
+	//		.attr("y2", this.linkCoords.bind(null, 'y2', machine))
+	//		.attr("opacity", function(d) {
+	//			if (d.type == NODE_LINK_TYPE.PIPED_IN) {
+	//				this.style.setProperty('opacity', 0)
+	//			}
+	//		})
+    //
+	//	this.layout.get(machine).alpha(.1)
+	//}
 }
 
 
