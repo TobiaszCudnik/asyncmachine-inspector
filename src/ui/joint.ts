3ed5a63f43983ac09a2ccf6f433a9c6e6b772cbc
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2017-04-14 13:38:52 -0400

    multiple links overlapping fixed

--- src/ui/joint.ts
+++ src/ui/joint.ts
@@ -14,6 +14,8 @@ import 'jointjs/src/vectorizer';
 declare const Vectorizer;
 import * as $ from 'jquery'
 import { throttle } from 'underscore'
+import g from '../vendor/geometry'
+import * as _ from 'underscore'
 import * as assert from 'assert/'
 import * as jsondiffpatch from 'jsondiffpatch'
 import * as debounce from 'throttle-debounce/debounce'
@@ -33,6 +35,9 @@ type IDelta = jsondiffpatch.IDeltas
 joint.shapes.fsa.Arrow = joint.shapes.fsa.Arrow.extend({
     markup: '<path class="connection"/><path class="marker-target"/><g class="labels" />'
 });
+
+// distance between multiple links
+const link_gap = 40
 
 /**
  * Fiddles:
@@ -128,9 +133,18 @@ export default class Ui extends UiBase<INetworkJson> {
 				interactive: {	
 					vertexAdd: false
 				}
-			});
+			})
 
 			this.bindMouseZoom()
+
+			var myAdjustVertices = _.partial(adjustVertices, this.graph);
+
+			// adjust vertices when a cell is removed or its source/target
+			// was changed
+			this.graph.on('add remove change:source change:target', myAdjustVertices);
+
+			// also when an user stops interacting with an element.
+			this.paper.on('cell:pointerup', myAdjustVertices);
 		}
 
 		if (this.data)
@@ -144,6 +158,8 @@ export default class Ui extends UiBase<INetworkJson> {
 	// TODO layout_data?
 	async setData(data: INetworkJson, layout_data,
 			changed_cells: Iterable<string> = null) {
+		const first_run = !this.data
+
 		this.data = data
 		let start = Date.now()
 
@@ -155,6 +171,11 @@ export default class Ui extends UiBase<INetworkJson> {
 				// TODO mutex on setdata till here
 				resolve()
 			}))
+		}
+
+		if (first_run) {
+			for (const cell of this.graph.getCells())
+				adjustVertices(this.graph, cell)
 		}
 
 		console.log(`Overall setData ${Date.now() - start}ms`)
@@ -424,3 +445,90 @@ export default class Ui extends UiBase<INetworkJson> {
 		return pointTransformed;
 	}
 }
+
+
+// TODO separate file
+// http://resources.jointjs.com/tutorial/multiple-links-between-elements
+function adjustVertices(graph, cell) {
+
+	// If the cell is a view, find its model.
+	cell = cell.model || cell;
+
+	if (cell instanceof joint.dia.Element) {
+
+		_.chain(graph.getConnectedLinks(cell)).groupBy(function(link) {
+			// the key of the group is the model id of the link's source or target, but not our cell id.
+			return _.omit([link.get('source').id, link.get('target').id], cell.id)[0];
+		}).each(function(group, key) {
+			// If the member of the group has both source and target model adjust vertices.
+			if (key !== 'undefined') adjustVertices(graph, _.first(group));
+		});
+
+		return;
+	}
+
+	// The cell is a link. Let's find its source and target models.
+	var srcId = cell.get('source').id || cell.previous('source').id;
+	var trgId = cell.get('target').id || cell.previous('target').id;
+
+	// If one of the ends is not a model, the link has no siblings.
+	if (!srcId || !trgId) return;
+
+	var siblings = _.filter(graph.getLinks(), function(sibling) {
+
+		var _srcId = sibling.get('source').id;
+		var _trgId = sibling.get('target').id;
+
+		return (_srcId === srcId && _trgId === trgId) || (_srcId === trgId && _trgId === srcId);
+	});
+
+	switch (siblings.length) {
+
+		case 0:
+			// The link was removed and had no siblings.
+			break;
+
+		case 1:
+			// There is only one link between the source and target. No vertices needed.
+			cell.unset('vertices');
+			break;
+
+		default:
+
+			// There is more than one siblings. We need to create vertices.
+
+			// First of all we'll find the middle point of the link.
+			var srcCenter = graph.getCell(srcId).getBBox().center();
+			var trgCenter = graph.getCell(trgId).getBBox().center();
+			var midPoint = g.line(srcCenter, trgCenter).midpoint();
+
+			// Then find the angle it forms.
+			var theta = srcCenter.theta(trgCenter);
+
+			// This is the maximum distance between links
+			var gap = link_gap;
+
+			_.each(siblings, function(sibling, index) {
+
+				// We want the offset values to be calculated as follows 0, 20, 20, 40, 40, 60, 60 ..
+				var offset = gap * Math.ceil(index / 2);
+
+				// Now we need the vertices to be placed at points which are 'offset' pixels distant
+				// from the first link and forms a perpendicular angle to it. And as index goes up
+				// alternate left and right.
+				//
+				//  ^  odd indexes
+				//  |
+				//  |---->  index 0 line (straight line between a source center and a target center.
+				//  |
+				//  v  even indexes
+				var sign = index % 2 ? 1 : -1;
+				var angle = g.toRad(theta + sign * 90);
+
+				// We found the vertex.
+				var vertex = g.point.fromPolar(offset, angle, midPoint);
+
+				sibling.set('vertices', [{ x: vertex.x, y: vertex.y }]);
+			});
+	}
+};
