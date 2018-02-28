538a91351cb6b378af1569eca5baa8e308becd20
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2018-02-28 13:37:39 -0500

    no prod console msgs in the worker

--- config/webpack-worker-prod.config.js
+++ config/webpack-worker-prod.config.js
@@ -1,11 +1,15 @@
 const config = require('./webpack-worker.config')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
 
 config.plugins.push(
   new UglifyJSPlugin({
     uglifyOptions: {
       ecma: 8
     }
+  }),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production')
   })
 )
 config.devtool = 'none'
