d08a25e298b5112855d7f37ad20dc69c504647fa
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2018-03-29 11:56:57 -0400

    removed old node typing

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,7 +6,6 @@
 /// <reference path="globals/eventemitter3/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/socket.io-client/index.d.ts" />
 /// <reference path="globals/socket.io/index.d.ts" />
 /// <reference path="globals/underscore/index.d.ts" />
